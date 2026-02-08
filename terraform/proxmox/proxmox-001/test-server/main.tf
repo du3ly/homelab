3c661c205bded5118cdaa891a1cbc063e4296208
Duely Yung <dyung2@gmail.com>
2026-02-07 22:55:12 -0600

    fmt

--- terraform/proxmox/proxmox-001/test-server/main.tf
+++ terraform/proxmox/proxmox-001/test-server/main.tf
@@ -1,8 +1,8 @@
 module "instance" {
   source = "../../../modules/proxmox-vm"
 
-  vm_name           = "test-server-terraform"
-  vm_id             = 102
+  vm_name = "test-server-terraform"
+  vm_id   = 102
 
-  vm_clone_template       = 201 # debian-12.13.0
+  vm_clone_template = 201 # debian-12.13.0
 }
