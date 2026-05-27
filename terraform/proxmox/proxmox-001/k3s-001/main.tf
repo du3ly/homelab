18d12656140c3231feeec6a158ff0468488f1652
Duely Yung <dyung2@gmail.com>
2026-05-26 22:34:19 -0500

    chore(k3s): consolidate into k3s-001

--- terraform/proxmox/proxmox-001/k3s-001/main.tf
+++ terraform/proxmox/proxmox-001/k3s-001/main.tf
@@ -2,9 +2,16 @@ module "instance" {
   source = "../../../modules/proxmox-vm"
 
   vm_name           = "k3s-001"
-  vm_clone_template = "debian-12.13.0"
+  vm_clone_template = "debian-13.4.0"
   full_clone        = false
 
   cores  = 2
   memory = 4096
+
+  user_data = <<EOF
+#cloud-config
+runcmd:
+  - apt-get update && apt-get install -y curl
+  - curl -sfL https://get.k3s.io | sh -
+EOF
 }
